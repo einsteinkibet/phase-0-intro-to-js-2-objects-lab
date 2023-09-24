868ab1218bab5721cb6f995a35521203ca0849cd
einsteinkibet <einsteinkibet0110@gmail.com>
2023-09-24 23:15:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+    name: "Yegon",
+    streetAddress: "20400 Bmt"
+};
+
+// Function to update employee with a new key-value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+// Function to update employee destructively and return the entire updated employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Function to delete a key from a clone of employee and returns the new employee
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+// Function to delete a key from employee destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
